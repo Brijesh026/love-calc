480e42a2c1600d1446f9dc36af5b112bfc0497b1
Brijesh026 <brijesh026@gmail.com>
2020-04-23 23:54:58 +0530

    Updates

---
+++ precache-manifest.574a37c108da90badb37e9ec31c5d520.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8881767e446da9ac2d7e",
+    "url": "/love-calc/static/js/app.86961ee8.chunk.js"
+  },
+  {
+    "revision": "c0e5af1a8f5628644101",
+    "url": "/love-calc/static/js/runtime~app.1619d89b.js"
+  },
+  {
+    "revision": "f9e65878251b1edd2a49",
+    "url": "/love-calc/static/js/2.ad4f62d2.chunk.js"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/love-calc/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/love-calc/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/love-calc/serve.json"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/love-calc/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/love-calc/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/love-calc/favicon.ico"
+  },
+  {
+    "revision": "4c2024a6f9612e3b128fc6ea86702c64",
+    "url": "/love-calc/index.html"
+  },
+  {
+    "revision": "2814c50ade95c4c483e008def7d24aed",
+    "url": "/love-calc/manifest.json"
+  },
+  {
+    "revision": "a6664ce0b682dcbcb7ef2b788925b620",
+    "url": "/love-calc/register-service-worker.js"
+  }
+];
